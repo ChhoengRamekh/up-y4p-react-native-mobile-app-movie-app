1a8f2f076b302e58889430cd51a06a34bbb0d79a
Ramekh Chhoeng <ramekhchhoeng023@gmail.com>
2021-08-02 12:56:18 +0700

    First initial project

---
+++ components/HomeCategory/styles.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+    image: {
+        width: 100,
+        height: 170,
+        resizeMode: 'cover',
+        borderRadius: 10,
+        margin: 5
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold'
+    }
+})
+
+export default styles
