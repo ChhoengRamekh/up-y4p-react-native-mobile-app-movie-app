6fe75a85ddc6cceb92442ed286ffc6e58d7e298d
Ramekh Chhoeng <ramekhchhoeng023@gmail.com>
2021-08-05 23:13:43 +0700

    amplify authentication(login/signup/logout)

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import Amplify from 'aws-amplify'
-
+import { withAuthenticator } from 'aws-amplify-react-native'
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
@@ -12,7 +12,7 @@ import Navigation from './navigation';
 import config from './src/aws-exports'
 Amplify.configure(config)
 
-export default function App() {
+function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
@@ -27,3 +27,5 @@ export default function App() {
     );
   }
 }
+
+export default withAuthenticator(App)
