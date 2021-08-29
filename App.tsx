3c982afe9eafa6aef3b5fb81c679ba92c0909755
Ramekh Chhoeng <ramekhchhoeng023@gmail.com>
2021-08-29 23:03:02 +0700

    update react component

--- App.tsx
+++ App.tsx
@@ -3,12 +3,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-import Amplify from 'aws-amplify'
 import { withAuthenticator } from 'aws-amplify-react-native'
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+
+import Amplify from 'aws-amplify'
 import config from './src/aws-exports'
 Amplify.configure(config)
 
